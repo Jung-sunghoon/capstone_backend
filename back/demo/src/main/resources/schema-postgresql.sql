0dfd32265564c97a08985995e651fa667f773d4f
KimMS <kimas1215@naver.com>
2023-09-14 14:19:36 +0900

    230914 자동 DB 생성

---
+++ back/demo/src/main/resources/schema-postgresql.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS capstone;
+CREATE DATABASE capstone;
+
+USE capstone;
+
+CREATE TABLE users (
+    userId VARCHAR(100) PRIMARY KEY,
+    password VARCHAR(100),
+    name VARCHAR(50),
+    nickname VARCHAR(50),
+    email VARCHAR(100),
+    gitAddress VARCHAR(100)
+);
+
+
+CREATE TABLE projectGenerate (
+    projectId int PRIMARY KEY,
+    projectTitle VARCHAR(100),
+    description VARCHAR(500),
+    creatorId VARCHAR(50),
+    recruitmentStatus VARCHAR(50),
+    recruitmentCount int,
+    generateDate varchar(50),
+    acceptedID VARCHAR(200)
+);
+
