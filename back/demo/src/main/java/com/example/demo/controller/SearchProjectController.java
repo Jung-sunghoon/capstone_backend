555d5331cb3af28a6da32b5c1bd7f5fdd84f21f0
KimMS <kimas1215@naver.com>
2023-09-19 01:27:37 +0900

    230919 프로젝트 검색 기능

---
+++ back/demo/src/main/java/com/example/demo/controller/SearchProjectController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.example.demo.dao.ProjectSearchDAO;
+import com.example.demo.dto.ProjectGenerateDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+import static java.sql.Types.NULL;
+
+@RestController
+@RequestMapping("/api")
+public class SearchProjectController {
+
+    @Autowired
+    private ProjectSearchDAO projectSearchDAO;
+
+
+    @PostMapping("/search_project")
+    public ResponseEntity<?> projectSearch(@RequestParam String projectTitle) {
+
+        List<ProjectGenerateDTO> results = projectSearchDAO.SearchProject(projectTitle);
+
+        if (results.isEmpty()) {
+            return new ResponseEntity<>("검색 결과가 없습니다", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(results, HttpStatus.OK);
+    }
+}
---
+++ back/demo/src/main/java/com/example/demo/dao/ProjectSearchDAO.java
@@ -0,0 +1,12 @@
+package com.example.demo.dao;
+
+
+import com.example.demo.dto.ProjectGenerateDTO;
+
+import java.util.List;
+
+public interface ProjectSearchDAO {
+
+    List<ProjectGenerateDTO> SearchProject(String projectTitle);
+
+}
