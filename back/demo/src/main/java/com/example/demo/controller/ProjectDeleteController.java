c5b65c75445b2267e57c34a0764e84fc39de5e3c
KimMS <kimas1215@naver.com>
2023-09-26 01:22:15 +0900

    230926 프로젝트 삭제 기능 추가

---
+++ back/demo/src/main/java/com/example/demo/controller/ProjectDeleteController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.dao.ProjectDeleteDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
+public class ProjectDeleteController {
+    @Autowired
+    private ProjectDeleteDAO projectDeleteDAO;
+
+    @DeleteMapping("/delete_project")
+    public ResponseEntity<?> deleteProject(@RequestParam int projectId) {
+        try {
+            projectDeleteDAO.deleteProject(projectId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+}
---
+++ back/demo/src/main/java/com/example/demo/dao/ProjectDeleteDAO.java
@@ -0,0 +1,5 @@
+package com.example.demo.dao;
+
+public interface ProjectDeleteDAO {
+    void deleteProject(int projectId);
+}
