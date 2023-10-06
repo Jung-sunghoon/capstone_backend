511e709e6ce7157857153626c02deb5875a6892f
KimMS <kimas1215@naver.com>
2023-10-06 16:28:54 +0900

    231006 단순 오타 수정

--- back/demo/src/main/java/com/example/demo/controller/IdDuplicateCheck.java
+++ back/demo/src/main/java/com/example/demo/controller/IdDuplicateCheck.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class IdDuplicateCheck {
@@ -16,7 +17,7 @@ public class IdDuplicateCheck {
     private SignUpDAO signUpDAO;
 
 
-    @GetMapping("/DuplicateCheck")
+    @GetMapping("/IdDuplicateCheck")
     public ResponseEntity<String> checkID(@RequestParam String id) {
         if (signUpDAO.checkUserId(id) > 0) {
             return ResponseEntity.status(402).body("이미 사용 중인 아이디입니다.");
