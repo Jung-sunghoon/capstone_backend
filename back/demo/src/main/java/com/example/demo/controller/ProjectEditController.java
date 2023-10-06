cde8daaa476e7b5bad1a8021b94d5a7567f610b2
KimMS <kimas1215@naver.com>
2023-10-06 16:36:57 +0900

    231006 프로젝트 수정시 기술 스택도 수정 가능하게 추가

--- back/demo/src/main/java/com/example/demo/controller/ProjectEditController.java
+++ back/demo/src/main/java/com/example/demo/controller/ProjectEditController.java
@@ -1,11 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.dao.ProjectEditDAO;
+import com.example.demo.dto.ProjectEditRequest;
 import com.example.demo.dto.ProjectGenerateDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -15,12 +19,38 @@ public class ProjectEditController {
     private ProjectEditDAO projectEditDAO;
 
     @PutMapping("/update_project")
-    public ResponseEntity<?> editProject(@RequestBody ProjectGenerateDTO project) {
+    public ResponseEntity<?> editProject(@RequestBody ProjectEditRequest request) {
         try {
+            ProjectGenerateDTO project = request.getProjectInfo();
+            if(project.getProjectStatus().equals("Ps_co")){
+                projectEditDAO.increasePointComplete(project.getUserId());
+            }
             projectEditDAO.EditProject(project);
+
+            // 기존 techIds 가져오기
+            List<Integer> existingTechIds = projectEditDAO.getTechStacksByProjectId(project.getProjectId());
+
+            // 비교 & 업데이트
+            List<Integer> newTechIds = request.getTechIds();
+
+            // 삭제할 techIds 찾기: existing에는 있지만 newTechIds에는 없는 아이디
+            List<Integer> toDelete = existingTechIds.stream()
+                    .filter(id -> !newTechIds.contains(id))
+                    .collect(Collectors.toList());
+            // 추가할 techIds 찾기: newTechIds에는 있지만 existing에는 없는 아이디
+            List<Integer> toAdd = newTechIds.stream()
+                    .filter(id -> !existingTechIds.contains(id))
+                    .collect(Collectors.toList());
+
+            // DB 업데이트
+            toDelete.forEach(id -> projectEditDAO.deleteTechId(project.getProjectId(), id));
+            toAdd.forEach(id -> projectEditDAO.addTechId(project.getProjectId(), id));
+
             return new ResponseEntity<>(HttpStatus.OK);
+
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
 }
--- back/demo/src/main/java/com/example/demo/dao/ProjectEditDAO.java
+++ back/demo/src/main/java/com/example/demo/dao/ProjectEditDAO.java
@@ -2,6 +2,13 @@ package com.example.demo.dao;
 
 import com.example.demo.dto.ProjectGenerateDTO;
 
+import java.util.List;
+
 public interface ProjectEditDAO {
     void EditProject(ProjectGenerateDTO project);
+    void increasePointComplete(String userId);
+
+    List<Integer> getTechStacksByProjectId(int projectId);
+    void addTechId(int projectId, int techId);
+    void deleteTechId(int projectId, int techId);
 }
