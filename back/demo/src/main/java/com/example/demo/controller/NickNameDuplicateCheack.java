5c232313b18612136b0fb36b171dabebe4acbe7e
KimMS <kimas1215@naver.com>
2023-10-06 16:24:57 +0900

    231006 닉네임 중복 체크 기능

---
+++ back/demo/src/main/java/com/example/demo/controller/NickNameDuplicateCheack.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.dao.SignUpDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
+public class NickNameDuplicateCheack {
+    @Autowired
+    private SignUpDAO signUpDAO;
+
+
+    @GetMapping("/NickNameDuplicateCheck")
+    public ResponseEntity<String> checkNickName(@RequestParam String nickName) {
+        if (signUpDAO.checkNickName(nickName) > 0) {
+            return ResponseEntity.status(402).body("이미 사용 중인 닉네임입니다.");
+        }
+        return ResponseEntity.ok("사용 가능합니다.");
+    }
+}
