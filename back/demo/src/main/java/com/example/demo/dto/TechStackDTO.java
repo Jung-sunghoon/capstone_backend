51c33d608b74504a4dbc260d8a6c90c98d2ebd2d
jisunggi <wltjd5316@naver.com>
2023-10-25 00:09:23 +0900

    231025 TechStackDto 수정

--- back/demo/src/main/java/com/example/demo/dto/TechStackDTO.java
+++ back/demo/src/main/java/com/example/demo/dto/TechStackDTO.java
@@ -4,4 +4,20 @@ public class TechStackDTO {
 
     private int techId;
     private String techName;
+
+    public int getTechId() {
+        return techId;
+    }
+
+    public void setTechId(int techId) {
+        this.techId = techId;
+    }
+
+    public String getTechName() {
+        return techName;
+    }
+
+    public void setTechName(String techName) {
+        this.techName = techName;
+    }
 }
