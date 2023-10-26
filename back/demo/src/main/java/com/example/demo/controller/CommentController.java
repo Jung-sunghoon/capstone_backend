4476d4419fc049f1d665b0582f6f7024f23813a7
jisunggi <wltjd5316@naver.com>
2023-10-27 02:28:15 +0900

    231027 CommentController 수정

--- back/demo/src/main/java/com/example/demo/controller/CommentController.java
+++ back/demo/src/main/java/com/example/demo/controller/CommentController.java
@@ -47,7 +47,7 @@ public class CommentController {
         return ResponseEntity.ok(comments);
     }
 
-    @PutMapping("/comments_edit/{projectId}")
+    @PutMapping("/comments_edit/{commentId}")
     public ResponseEntity<String> updateComment(@PathVariable int commentId, @RequestBody CommentDTO comment) {
         // CommentDTO 객체에 commentToUpdateId 설정
         comment.setCommentToUpdateId(commentId);
@@ -58,7 +58,7 @@ public class CommentController {
         return ResponseEntity.ok("댓글 수정 완료");
     }
 
-    @DeleteMapping("/comments_delete/{projectId}")
+    @DeleteMapping("/comments_delete/{commentId}")
     public ResponseEntity<String> deleteComment(@PathVariable int commentId, @RequestParam String projectGenerationUserId, @RequestParam String userId) {
         // CommentDAO를 사용하여 comment를 삭제하는 코드 추가
         commentDAO.deleteComment(commentId);
