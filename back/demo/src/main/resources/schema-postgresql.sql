995142e4a6f6c56ab0fc756c5bc6d71ec6a239e7
KimMS <kimas1215@naver.com>
2023-11-22 22:50:22 +0900

    231122 IT정보 DB 추가

--- back/demo/src/main/resources/schema-postgresql.sql
+++ back/demo/src/main/resources/schema-postgresql.sql
@@ -75,5 +75,5 @@ CREATE TABLE pass (
 CREATE TABLE ItInformation(
     title VARCHAR(100),            /* IT 정보 제목 */
     description LONGTEXT,          /* IT 정보 내용 */
-    generateDate varchar(50)       /* IT 정보 내용 */
+    generateDate varchar(50)       /* IT 정보 생성일 */
 );
